11dec25a425644ded3c25e7030b6598c30ca9844
HakeemAzeez <22ftt1511@student.pb.edu.bn>
2024-03-19 08:47:19 +0800

    22ftt1511

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -909,7 +909,7 @@
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                 <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                     <div class="flex lg:justify-center lg:col-start-2">
-
+                        <h1 style="color:red;">22FTT1511</h1>
                     </div>
                     @if (Route::has('login'))
                     <nav class="-mx-3 flex flex-1 justify-end">
